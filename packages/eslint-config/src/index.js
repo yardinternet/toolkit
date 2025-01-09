759ea137f2cc8bb6bcabb3c17664205ff1ca00eb
Freek de Kruijff <freek.dekruijff@yard.nl>
2025-01-09 15:55:15 +0100

    (chore): adda auto generated settings from migration tool

---
+++ packages/eslint-config/src/index.js
@@ -0,0 +1,52 @@
+const globals = require("globals");
+const babelParser = require("@babel/eslint-parser");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = [
+    ...compat.extends("plugin:@wordpress/eslint-plugin/recommended", "prettier"),
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                CLI: "readonly",
+                wp: "readonly",
+            },
+
+            parser: babelParser,
+            ecmaVersion: 5,
+            sourceType: "commonjs",
+
+            parserOptions: {
+                requireConfigFile: false,
+
+                babelOptions: {
+                    presets: ["@babel/preset-react"],
+                },
+
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+
+        rules: {
+            "prettier/prettier": 0,
+            "jsdoc/require-param": 0,
+
+            "no-unused-expressions": ["error", {
+                allowTernary: true,
+            }],
+        },
+    },
+];
