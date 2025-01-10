7c37f3e272fc4c0de20da7ec131a090ae1d2a66b
Freek de Kruijff <freek.dekruijff@yard.nl>
2025-01-10 11:53:36 +0100

    (docs): getPathToTheme

--- packages/postcss-config/README.md
+++ packages/postcss-config/README.md
@@ -18,10 +18,11 @@ const postcssSettings = require('@yardinternet/postcss-config');
 module.exports = postcssSettings();
 ```
 
-The default path to the styles is `web/app/themes/sage/resources/styles`. 
-You can change this value by passing the path as an parameter.
+The default path to the styles is `web/app/themes/sage/resources/styles`.
+The theme path gets set by the getPathToTheme() function from Yard scripts.
+You can change these values by passing the paths as an parameter.
 
 ```js
 const postcssSettings = require('@yardinternet/postcss-config');
-module.exports = postcssSettings('resources/styles');
+module.exports = postcssSettings('resources/styles', 'src/theme');
 ```
--- packages/postcss-config/src/index.js
+++ packages/postcss-config/src/index.js
@@ -1,7 +1,7 @@
 const { getPathToTheme } = require( './scripts/helpers' );
 const path = require( 'path' );
 
-module.exports = (pathToStyles = 'web/app/themes/sage/resources/styles') => {
+module.exports = (pathToStyles = 'web/app/themes/sage/resources/styles', pathToTheme = getPathToTheme()) => {
 
 	// Add alias paths for PostCSS imports
 	const resolveAliasPaths = ( id ) => {
@@ -28,7 +28,7 @@ module.exports = (pathToStyles = 'web/app/themes/sage/resources/styles') => {
 			'tailwindcss/nesting': {},
 			'postcss-nested': {}, // We want nested rules the same way Sass works. Also needed for postcss-mixins.
 			tailwindcss: {
-				config: path.resolve( getPathToTheme(), 'tailwind.config.cjs' ),
+				config: path.resolve( pathToTheme, 'tailwind.config.cjs' ),
 			},
 		};
 
