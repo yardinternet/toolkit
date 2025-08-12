05dc886530430f88c0fafd248b6ee637296aa151
Freek de Kruijff <freek.dekruijff@yard.nl>
2025-08-12 14:14:42 +0200

    Revert "(chore): remove babel"
    This reverts commit 9ce16b13b0aecae9f2e7b086667e6c8392c82f2a.

--- packages/eslint-config/src/index.js
+++ packages/eslint-config/src/index.js
@@ -1,4 +1,6 @@
 const globals = require( 'globals' );
+// eslint-disable-next-line import/no-extraneous-dependencies
+const babelParser = require( '@babel/eslint-parser' );
 const js = require( '@eslint/js' );
 
 const { FlatCompat } = require( '@eslint/eslintrc' );
@@ -23,8 +25,14 @@ module.exports = [
 				wp: 'readonly',
 			},
 
+			parser: babelParser,
+
 			parserOptions: {
 				requireConfigFile: false,
+
+				babelOptions: {
+					presets: [ '@babel/preset-react' ],
+				},
 
 				ecmaFeatures: {
 					jsx: true,
