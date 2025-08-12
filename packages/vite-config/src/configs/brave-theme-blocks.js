e6dd7d888409a916cabb836a1586bb3961b39a94
Freek de Kruijff <freek.dekruijff@yard.nl>
2025-08-12 13:51:53 +0200

    (fix): please eslint by givning a full path

--- packages/vite-config/src/configs/brave-theme-blocks.js
+++ packages/vite-config/src/configs/brave-theme-blocks.js
@@ -11,7 +11,7 @@
  */
 import { defineConfig } from 'vite';
 import { createViteBlock } from 'vite-plugin-gutenberg-blocks';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite/dist/index.mjs';
 import path from 'path';
 
 /**
