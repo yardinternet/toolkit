0427338b5818b15888baef358df7e3e8dd6dc01f
YvetteNikolov <yvettenikolov@gmail.com>
2026-04-08 11:10:33 +0200

    chore: add more pointers to pick up later

--- packages/vite-config/src/configs/laravel-package.js
+++ packages/vite-config/src/configs/laravel-package.js
@@ -2,7 +2,7 @@ import { createBasePackageConfig } from './base-package.js';
 
 export const laravelPackageConfig = ( options = {} ) =>
 	createBasePackageConfig( {
-		outDir: 'public/build',
+		outDir: 'public/build', // TODO: do we need to really use build here? maybe just public?
 		formats: [ 'es', 'cjs' ],
 		manifest: true,
 		...options,
