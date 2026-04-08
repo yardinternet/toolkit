a7ff616d17c396625e1675733804a059281df310
YvetteNikolov <yvettenikolov@gmail.com>
2026-04-08 17:11:06 +0200

    chore: progress

--- packages/vite-config/src/configs/laravel-package.js
+++ packages/vite-config/src/configs/laravel-package.js
@@ -2,8 +2,7 @@ import { createBasePackageConfig } from './base-package.js';
 
 export const laravelPackageConfig = ( options = {} ) =>
 	createBasePackageConfig( {
-		outDir: 'public/build', // TODO: do we need to really use build here? maybe just public?
-		formats: [ 'es', 'cjs' ],
+		outDir: 'public',
 		manifest: true,
 		...options,
 	} );
