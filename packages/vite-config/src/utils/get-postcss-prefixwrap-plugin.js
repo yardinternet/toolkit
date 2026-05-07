86496e74d92a397ebe516abe2deb271d144cd4c8
Yannic van Veen <yannic.vanveen@yard.nl>
2026-05-07 10:03:37 +0200

    feat: add configuration for postcss prefixwrap ignored selectors

--- packages/vite-config/src/utils/get-postcss-prefixwrap-plugin.js
+++ packages/vite-config/src/utils/get-postcss-prefixwrap-plugin.js
@@ -1,22 +1,28 @@
 import postcssPrefixWrap from 'postcss-prefixwrap';
 
+const DEFAULT_IGNORED_SELECTORS = [
+	':root',
+	/^body.+$/,
+	/^\.editor-styles-wrapper.+$/,
+];
+
 /**
  * Conditionally returns the PostCSS Prefix Wrap plugin. This plugin is used to wrap editor styles
- * inside a .editor-styles-wrapper class based on the provided entrypooints. Some selectors are ignored
+ * inside a .editor-styles-wrapper class based on the provided entrypoints. Some selectors are ignored
  * because they need to remain global.
  */
 export function getPostCssPrefixWrapPlugin( config = {} ) {
-	if ( ! Array.isArray( config?.entryPoints ) ) {
+	if ( ! Array.isArray( config.entryPoints ) ) {
 		return [];
 	}
 
+	const ignoredSelectors = Array.isArray( config.ignoredSelectors )
+		? [ ...DEFAULT_IGNORED_SELECTORS, ...config.ignoredSelectors ]
+		: DEFAULT_IGNORED_SELECTORS;
+
 	return [
 		postcssPrefixWrap( '.editor-styles-wrapper', {
-			ignoredSelectors: [
-				':root',
-				/^(body)(.+)$/,
-				/^(.editor-styles-wrapper)(.+)$/,
-			],
+			ignoredSelectors,
 			prefixRootTags: false,
 			whitelist: config.entryPoints,
 		} ),
