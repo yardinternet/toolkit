9084f9f4ef2663c291854f23409b9279e0d76d53
YvetteNikolov <yvettenikolov@gmail.com>
2026-06-08 16:05:02 +0200

    refactor: remove deprecated workspace file and create vitest.config.mjs

--- vitest.workspace.mjs
+++
@@ -1,6 +0,0 @@
-import { defineWorkspace } from 'vitest/config';
-
-export default defineWorkspace([
-  'packages/eslint-config/vitest.config.mjs',
-  'packages/prettier-config/vitest.config.mjs',
-]);
